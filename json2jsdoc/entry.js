c4d2b62fe85fb3993204d9d907b826bcce9fcbfe
0x0a0d <vandug23@gmail.com>
2019-08-06 12:48:07 +0700

    json2jsdoc

---
+++ json2jsdoc/entry.js
@@ -0,0 +1,7 @@
+import 'jquery';
+import 'bootstrap/dist/js/bootstrap.min';
+import './js/json2doc'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'codemirror/lib/codemirror.css'
+import './css/custom.css';
---
+++ json2jsdoc/js/json2doc.js
@@ -0,0 +1,38 @@
+import $ from 'jquery';
+import CodeMirror from 'codemirror/lib/codemirror'
+import 'codemirror/mode/javascript/javascript'
+import Json2JSDoc from 'json2jsdoc';
+
+$(function () {
+  const defaultSize = ( window.innerWidth < 768 ) ? 200 : 400;
+
+  const input = CodeMirror.fromTextArea(document.querySelector('#json-input'), {
+    lineNumbers: true,
+    json: true,
+    styleActiveLine: true
+  });
+  const output = CodeMirror.fromTextArea(document.querySelector('#json-output'), {
+    lineNumbers: true,
+    styleActiveLine: true
+  });
+  input.setSize('100%', defaultSize);
+  output.setSize('100%', defaultSize);
+  $('#btn-convert').on('click', function (e) {
+    e.preventDefault();
+    try {
+      const json = JSON.parse(input.getValue());
+      const namespace = $('#namespace').val();
+      const memberOf = $('#memberOf').val();
+      const converter = new Json2JSDoc(json, {
+        namespace: namespace === '' ? 'json2JSDoc' : namespace,
+        memberOf,
+        break_line: '\r\n'
+      });
+      const jsdoc = converter.convert().export();
+      output.setValue(jsdoc);
+      $('#json-output').val(jsdoc);
+    } catch (e) {
+      console.error(e);
+    }
+  })
+});
