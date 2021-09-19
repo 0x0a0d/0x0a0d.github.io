7d267a794b253562f03397c1c0c27730b7a0f190
0x0a0d <vandug23@gmail.com>
2021-09-19 08:00:14 +0700

    release 1.0

--- json2jsdoc/entry.js
+++
@@ -1,7 +0,0 @@
-import 'jquery';
-import 'bootstrap/dist/js/bootstrap.min';
-import './js/json2doc'
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'codemirror/lib/codemirror.css'
-import './css/custom.css';
